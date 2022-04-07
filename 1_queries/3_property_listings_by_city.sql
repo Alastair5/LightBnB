92de89b17909cf20ca0b407836eb53644de14989
Alastair <alastair05@gmail.com>
2022-04-06 19:32:33 -0700

    Add queries and new information for tables

---
+++ 1_queries/3_property_listings_by_city.sql
@@ -0,0 +1,7 @@
+SELECT properties.id, title, cost_per_night, AVG(property_reviews.rating) AS average_rating
+FROM properties
+JOIN property_reviews ON property_id = properties.id
+WHERE city LIKE '%ancouv%'
+GROUP BY properties.id
+HAVING AVG(property_reviews.rating) >= 4
+ORDER BY properties.cost_per_night;
