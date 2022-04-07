493ab984b13f51877a4b2a63617c751ee6f94e50
Alastair <alastair05@gmail.com>
2022-04-07 15:52:14 -0700

    Update database to return promises and populate tables

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -7,7 +7,7 @@ const pool = new Pool({
   user: 'vagrant',
   password: '123',
   host: 'localhost',
-  database: 'bootcampx'
+  database: 'lightbnb'
 });
 
 pool.query(`SELECT title FROM properties LIMIT 10;`).then(response => {
@@ -23,16 +23,12 @@ pool.query(`SELECT title FROM properties LIMIT 10;`).then(response => {
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
+  return pool.query(`SELECT id, name, email, password FROM users WHERE email = $1;`, [email.toLowerCase()])
+    .then((result) => result.rows[0])
+    .catch((err) => {
+      console.log(err);
+      return null;
+    });
 };
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -42,7 +38,13 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  return pool
+    .query(`SELECT id, name, email, password FROM users WHERE id = $1;`, [id])
+    .then((result) => result.rows[0])
+    .catch((err) => {
+      console.log(err);
+      return null;
+    });
 };
 exports.getUserWithId = getUserWithId;
 
@@ -53,10 +55,10 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  return pool
+    .query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *;`, [user['name'], user['email'], user['password']])
+    .then((result) => result.rows)
+    .catch((err) => console.log(err));
 };
 exports.addUser = addUser;
 
